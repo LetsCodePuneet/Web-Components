f842173d6b3db9b255bfc4076afe8908fecbf754
Arora, Puneet <puneetarora@deloitte.com>
2025-02-17 19:35:40 +0530

    Counter: Added logger to track the value

--- Counter/js/index.js
+++ Counter/js/index.js
@@ -2,3 +2,7 @@ import Counter from "./components/counter.js";
 
 // there should be '- in the comonent name. It is mandatory
 customElements.define('counter-component', Counter);
+
+const myCounter = document.getElementById("my-counter");
+
+setTimeout(()=>console.log(myCounter.value), 2000)
